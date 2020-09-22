8c4d79a4a024ba72187530b3e284f9f854e9924f
ma <m.fuica01@ufromail.cl>
2020-09-21 23:18:07 -0300

    Agrege comentario para demo

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,6 +9,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
+    // este es un comentario
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
