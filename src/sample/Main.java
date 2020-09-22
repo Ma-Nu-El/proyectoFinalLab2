a72f5d61e0a8b3dd006f7175b13d508d6970e542
ma <m.fuica01@ufromail.cl>
2020-09-21 23:27:50 -0300

    Segundo comentario para segundo demo

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,6 +10,7 @@ public class Main extends Application {
 
     @Override
     // este es un comentario
+    // otro comentario para el segundo demo
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
